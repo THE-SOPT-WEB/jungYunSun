6c1523135b9b58fe37fc7b86b0be382a1fe504f7
yunsun99 <79686751+yunsun99@users.noreply.github.com>
2022-04-22 09:15:09 +0900

    feat: add function to add/delete burger in cart

---
+++ 01_order/app.js
@@ -0,0 +1,48 @@
+const burgers = document.querySelectorAll('article');
+
+function handleBurgerDeleteClick(e) {
+  const burgerInCart = e.target.parentElement;
+  burgerInCart.remove();
+}
+
+function addBurgerToCart(clickedBurger) {
+  const burgerInCart = document.createElement("div");
+  const burgerName = document.createElement("div");
+  const burgerCount = document.createElement("input");
+  const burgerPrice = document.createElement("div");
+  const burgerDelete = document.createElement("button");
+
+  burgerInCart.classList.add("burger_in_cart");
+  burgerName.classList.add("burger_name");
+  burgerCount.classList.add("burger_count");
+  burgerCount.type = "number"
+  burgerPrice.classList.add("burger_price");
+  burgerDelete.classList.add("burger_delete");
+
+  const cart = document.querySelector(".cart_section");
+  const totalPrice = document.querySelector('.total_price > div');
+  
+
+  burgerName.innerText = clickedBurger.querySelector("h2").innerText;
+  burgerCount.value = 1;
+  burgerPrice.innerText = clickedBurger.querySelector("h3").innerText;
+  burgerDelete.innerText = "X"
+
+  burgerDelete.addEventListener('click', handleBurgerDeleteClick);
+
+  burgerInCart.appendChild(burgerName);
+  burgerInCart.appendChild(burgerCount);
+  burgerInCart.appendChild(burgerPrice);
+  burgerInCart.appendChild(burgerDelete);
+  cart.appendChild(burgerInCart);
+
+}
+
+function handleBurgerClick(e) {
+  const clickedBurger = e.currentTarget;
+  addBurgerToCart(clickedBurger);
+}
+
+burgers.forEach(burger => {
+  burger.addEventListener('click', handleBurgerClick);
+});
