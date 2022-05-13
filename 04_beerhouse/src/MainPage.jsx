e539fbb4c73e7202944178bbb3e8e360cc790714
yunsun99 <79686751+yunsun99@users.noreply.github.com>
2022-05-13 21:56:29 +0900

    implement basic function

--- 04_beerhouse/src/MainPage.jsx
+++ 04_beerhouse/src/MainPage.jsx
@@ -1,23 +1,97 @@
 import styled from 'styled-components';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import BeerHouse from './BeerHouse';
 
-function handleButtonClick() {}
+const getLocation = (errHandler) => {
+  if ('geolocation' in navigator) {
+    return new Promise((resolve) => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const {
+            coords: { latitude: y, longitude: x },
+          } = position;
+          resolve({ x, y });
+        },
+        (e) => {
+          alert('HTTPS 연결을 확인해주세요.');
+          errHandler && errHandler();
+        }
+      );
+    });
+  }
+
+  return { x: 126.8350976, y: 37.617664 };
+};
+
+async function 위치가져오기() {
+  const result = await getLocation();
+  const { x, y } = result;
+  return { x, y };
+}
+
+async function 내근처맥주집가져오기() {
+  const currentLocation = await 위치가져오기();
+  const result = await axios.get(
+    'https://dapi.kakao.com/v2/local/search/keyword',
+    {
+      headers: {
+        Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_AK}`,
+      },
+      params: {
+        x: currentLocation.x,
+        y: currentLocation.y,
+        radius: 1000,
+        query: '맥주',
+      },
+    }
+  );
+  return result.data.documents;
+}
 
 function MainPage() {
+  const [nearBeerHouses, setNearBeerHouses] = useState([]);
+  const [checkboxIsChecked, setCheckboxIsChecked] = useState(false);
+
+  async function handleButtonClick() {
+    const receivedNearBeerHouse = await 내근처맥주집가져오기();
+    checkboxIsChecked
+      ? setNearBeerHouses(receivedNearBeerHouse)
+      : setNearBeerHouses([]);
+  }
+
+  function handleCheckboxChange(e) {
+    setCheckboxIsChecked(e.target.checked);
+  }
+
   return (
     <MainWrapper>
       <Title>우리 동네 맥주집</Title>
       <LocationSection>
         <AutoSearch>
           <CurrentLocation>지역 기반으로 검색할게요</CurrentLocation>
-          <CheckBox type="checkbox" />
+          <CheckBox
+            type="checkbox"
+            checked={checkboxIsChecked}
+            onChange={handleCheckboxChange}
+          />
         </AutoSearch>
         <ManualSearch>
-          <div>우리 동네는 여기에요</div>
-          <input type="text" placeholder="지역을 입력해주세요."></input>
-          <button onClick={handleButtonClick}>검색하기</button>
+          <SearchingArea>우리 동네는 여기에요</SearchingArea>
+          <InputArea type="text" placeholder="지역을 입력해주세요." />
         </ManualSearch>
-        <SearchResult></SearchResult>
+        <SearchButton onClick={handleButtonClick}>검색하기</SearchButton>
       </LocationSection>
+      <SearchResult>
+        {nearBeerHouses.map((nearBeerHouse) => {
+          return (
+            <BeerHouse
+              beerHouseInfo={nearBeerHouse}
+              key={nearBeerHouse.place_name}
+            />
+          );
+        })}
+      </SearchResult>
     </MainWrapper>
   );
 }
@@ -33,6 +107,7 @@ const MainWrapper = styled.div`
 
 const Title = styled.header`
   font-size: 2rem;
+  font-weight: bold;
   padding: 25px 0;
 `;
 
@@ -47,6 +122,7 @@ const LocationSection = styled.section`
 
 const CurrentLocation = styled.div`
   font-size: 1.3rem;
+  font-weight: bold;
   margin-right: 3px;
 `;
 
@@ -65,8 +141,29 @@ const CheckBox = styled.input`
   display: block;
 `;
 
-const ManualSearch = styled.div``;
+const ManualSearch = styled.div`
+  margin-bottom: 20px;
+`;
 
-const SearchResult = styled.section``;
+const SearchingArea = styled.div`
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+
+const InputArea = styled.input`
+  display: block;
+`;
+
+const SearchButton = styled.button`
+  width: 120px;
+  font-size: 1.2rem;
+  padding: 10px;
+  border: 2px solid black;
+  border-radius: 20px;
+`;
+
+const SearchResult = styled.section`
+  padding: 20px;
+`;
 
 export default MainPage;
